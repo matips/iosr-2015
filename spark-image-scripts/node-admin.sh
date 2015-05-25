7b6330a90c9dfcdb129fa97b44a716f6a309c062
Rafał Słota <roxeon@gmail.com>
2015-05-25 22:19:58 +0200

    fix mesos-state

--- spark-image-scripts/node-admin.sh
+++ spark-image-scripts/node-admin.sh
@@ -2,10 +2,20 @@
 
 MASTER_FILE="/etc/cloud/master"
 
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+$(cd $DIR ; git pull)
+
 function usage {
     echo "Usage: `basename $0` start|stop master|slave [MASTER_HOSTNAME]"
     exit 1
-} 
+}
 
 [[ $# -ge 2 ]] || usage
 
